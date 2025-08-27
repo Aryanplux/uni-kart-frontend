7b8523668bd8c699539258bbae45adb1e5b4dcaf
Aryan Dhiman <146617531+Aryanplux@users.noreply.github.com>
2025-08-27 15:25:04 +0530

    Delete src directory

--- src/components/Onboarding/OnboardingContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WelcomeScreen from "./WelcomeScreen";
-
-const OnboardingContainer = ({ username, onComplete }) => (
-  <WelcomeScreen username={username} onFinish={onComplete} />
-);
-
-export default OnboardingContainer;
